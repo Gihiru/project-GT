e7be47189eff9fcc93a89884a058ae29fef03933
gavindu-gayashan <gavindukariyawasam@gmail.com>
2025-08-05 04:22:09 +0530

    add effects to logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <Link to="/">
-        <img src={assets.logo} className="w-36" alt="" />
+        <img src={assets.logo} className="w-36 logo-shine" alt="" />
       </Link>
 
       <ul className="hidden sm:flex gap-5 text-sm text-gray-100">
